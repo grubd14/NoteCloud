5d2501facf4f38eddf8877e137d108e7858a3325
grubd14 <sushilneupane9745@gmail.com>
2023-04-27 20:09:35 +0545

    add: register with firebase add: firebase initialization in main.dart fix: improve loginpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:note_cloud/screens/login_screen.dart';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      highContrastDarkTheme: ThemeData.dark(),
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const LoginScreen(),
-    ));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    themeMode: ThemeMode.dark,
+    highContrastDarkTheme: ThemeData.dark(),
+    theme: ThemeData(
+      useMaterial3: true,
+    ),
+    home: const LoginScreen(),
+  ));
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:note_cloud/firebase_options.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,21 +11,82 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Login Screen"),
         ),
-        body: Column(
-          children: [
-            TextField(),
-            TextField(),
-            TextButton(
-              onPressed: () async {},
-              child: const Text('Register'),
-            ),
-          ],
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _email,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.all(12),
+                            hintText: 'Enter your Email'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: TextField(
+                        controller: _password,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.all(12),
+                            hintText: 'Enter your password'),
+                      ),
+                    ),
+                    TextButton(
+                      style: const ButtonStyle(
+                          iconSize: MaterialStatePropertyAll(40),
+                          elevation: MaterialStatePropertyAll(34)),
+                      onPressed: () async {
+                        final email = _email.text;
+                        final password = _password.text;
+
+                        final userCredential = await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                          email: email,
+                          password: password,
+                        );
+                      },
+                      child: const Text('Register'),
+                    ),
+                  ],
+                );
+              default:
+                return const Text("Loading.......");
+            }
+          },
         ));
   }
 }
