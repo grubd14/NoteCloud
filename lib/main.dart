c835242b564d35fc13e7432833ba1d76c1affaf1
grubd14 <83056016+grubd14@users.noreply.github.com>
2023-05-04 10:33:39 +0545

    feat: email verfication using firebase. fix: show email verfied in console.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:note_cloud/screens/login_screen.dart';
+import "package:firebase_auth/firebase_auth.dart";
+import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +13,51 @@ void main() {
       colorScheme: const ColorScheme.dark(),
       useMaterial3: true,
     ),
-    home: const LoginScreen(),
+    home: const Homepage(),
   ));
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Home Page"),
+        ),
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                final user = FirebaseAuth.instance.currentUser;
+                if (user!.emailVerified) {
+                  print("You are verfied");
+                } else {
+                  print('Verify your Email Address');
+                }
+                return const Text('Done');
+              default:
+                return const Text("Loading.......");
+            }
+          },
+        ));
+  }
+}
+
+class EmailVerifyScreen extends StatefulWidget {
+  const EmailVerifyScreen({super.key});
+
+  @override
+  State<EmailVerifyScreen> createState() => _EmailVerifyScreenState();
+}
+
+class _EmailVerifyScreenState extends State<EmailVerifyScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
 }
